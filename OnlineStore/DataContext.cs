2219a729da81737a818c7610dcd6235635938873
Konrad Stępniak <vesphy@gmail.com>
2019-10-09 22:09:54 +0200

    Add DataContext

---
+++ OnlineStore/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace OnlineStore
+{
+  class DataContext
+  {
+    public List<Client> Clients;
+    public Dictionary<uint, Product> Products;
+    public List<Offer> Offers;
+    public ObservableCollection<Invoice> Invoices;
+  }
+}
