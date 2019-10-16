4f8d2a84b1c93a2aa23c277cfe8c3366010d28f9
Konrad Stępniak <vesphy@gmail.com>
2019-10-16 15:08:36 +0200

    Add ConsoleInterface project

---
+++ OnlineStore.ConsoleInterface/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.ConsoleInterface
+{
+  class DataService
+  {
+    private DataRepository _dataRepository;
+    public DataService(DataRepository dataRepository)
+    {
+      this._dataRepository = dataRepository;
+    }
+  }
+}
---
+++ OnlineStore.ConsoleInterface/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineStore.ConsoleInterface
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DataService dataService = new DataService(new DataRepository(new EmptyDataFiller()));
+    }
+
+    private class EmptyDataFiller : IDataFiller
+    {
+      public void Fill(DataContext context)
+      {
+      }
+    }
+  }
+}
